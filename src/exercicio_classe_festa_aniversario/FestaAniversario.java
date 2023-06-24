7accd9e8be20f3b4cabae1c839ac779bc1adec0a
soreziin <sorederozan@gmail.com>
2023-06-24 00:51:42 -0300

    first commit

---
+++ src/exercicio_classe_festa_aniversario/FestaAniversario.java
@@ -0,0 +1,8 @@
+package exercicio_classe_festa_aniversario;
+
+public class FestaAniversario {
+	public static void main(String[] args) {
+		AtributosFestaAniversario festa = new AtributosFestaAniversario("Pedro", 20, "Shopping Morumbi", "08/05/2024", 300);
+		festa.status();	
+	}
+}
